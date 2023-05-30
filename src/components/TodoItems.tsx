3efdd572c6bdbef47c300d31764517f398709ceb
sourabh jana <sourabh.jana@cubexo.io>
2023-05-30 19:03:36 +0530

    init commit

---
+++ src/components/TodoItems.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+type TodoItemProps = {
+    id: string
+    title: string
+    complete: boolean
+    toggleTodo: (id: string, complete: boolean) => void
+}
+
+
+
+export default function TodoItems({ id, title, complete, toggleTodo }: TodoItemProps) {
+    return <li className="flex gap-1 items-center">
+        <input
+            type="checkbox"
+            name={title}
+            id={id}
+            className="cursor-pointer peer"
+            defaultChecked={complete}
+            onChange={e => toggleTodo(id, e.target.checked)}
+        />
+        <label htmlFor={id} className="peer-checked:line-through cursor-pointer peer-checked:text-slate-500">{title}</label>
+    </li>
+}
